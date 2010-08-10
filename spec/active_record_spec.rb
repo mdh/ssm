31681ec896cf25f5f4e647f51bbe73e5d4bca133
Marek de Heus <marek@deheus.net>
2010-08-10 21:41:49 +0200

    Fixed active_record dependency

--- spec/active_record_spec.rb
+++ spec/active_record_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 require 'rubygems'
-gem 'activerecord', '>= 1.15.4.7794'
+gem 'activerecord', '~> 2.3.5'
 require 'active_record'
 require 'examples/user'
 
