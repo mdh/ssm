8d1e8d85776e0c038a4f85f15235675ae77e851e
Petrik <petrik@deheus.net>
2011-11-18 12:23:04 +0100

    Only require active record if ActiveREcord defined

--- lib/simple_state_machine.rb
+++ lib/simple_state_machine.rb
@@ -1,10 +1,13 @@
 require 'simple_state_machine/simple_state_machine'
 require 'simple_state_machine/state_machine'
+require 'simple_state_machine/tools/graphviz'
+require 'simple_state_machine/tools/inspector'
 require 'simple_state_machine/state_machine_definition'
 require 'simple_state_machine/transition'
 require 'simple_state_machine/decorator/default'
-require 'simple_state_machine/graphiz'
-require 'simple_state_machine/active_record'
-require 'simple_state_machine/decorator/active_record'
+if defined?(ActiveRecord)
+  require 'simple_state_machine/active_record'
+  require 'simple_state_machine/decorator/active_record'
+end
 require "simple_state_machine/railtie" if defined?(Rails::Railtie)
 
