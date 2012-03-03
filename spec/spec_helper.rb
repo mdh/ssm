243fe356e43384bd2aa2504b82071c72bf2281a9
Petrik <petrik@deheus.net>
2012-03-03 09:46:13 +0100

    Skip active record specs if not installed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
 require "rubygems"
 require "bundler"
-Bundler.require
-#Bundler.setup(:test)#, :activerecord)
-require 'active_record'
+Bundler.require :test
+#Bundler.setup(:test, :activerecord)
+begin
+  require 'active_record'
+rescue LoadError
+  puts "Skipping ActiveRecord specs"
+end
 
 ROOT = Pathname(File.expand_path(File.join(File.dirname(__FILE__), '..')))
 $LOAD_PATH << File.join(ROOT, 'lib')
@@ -13,5 +17,5 @@ require 'simple_state_machine'
 require File.join(ROOT, 'examples', 'conversation.rb')
 require File.join(ROOT, 'examples', 'lamp.rb')
 require File.join(ROOT, 'examples', 'traffic_light.rb')
-require File.join(ROOT, 'examples', 'user.rb')
+require File.join(ROOT, 'examples', 'user.rb') #if defined? ActiveRecord
 
